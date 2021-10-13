19e94730eb45c2fa1dcd9b52ecb194540f18349d
asvindra <aswin1399904@gmail.com>
2021-10-14 00:32:59 +0530

    image search app

--- block-BJaaju/code/index.js
+++ block-BJaaju/code/index.js
@@ -1,13 +1,25 @@
 let input =document.querySelector('input');
 let imgBox=document.querySelector('figure');
-
-input.addEventListener('keyup',()=>{
+//const url=`https://api.unsplash.com/search/photos/?client_id=p1-hM9mBQfTyRcIyt5p_e1Y2ciEpFRfmV7eKnAtAH3E&query${query}`;
+input.addEventListener('keyup',(e)=>{
+    imgBox.innerHTML="";
 
     let xhr=new XMLHttpRequest();
-    xhr.open('GET','https://api.unsplash.com/search/photos/');
+    xhr.open('GET',`https://api.unsplash.com/search/photos/?client_id=p1-hM9mBQfTyRcIyt5p_e1Y2ciEpFRfmV7eKnAtAH3E&query=${e.target.value}`);
     xhr.onload=function (){
       let data=JSON.parse(xhr.response);
-      imgBox.src=data.url;
+      //imgBox.src=data.url;
+      console.log(data.results);
+      data.results.forEach(ele=>{
+        let li=document.createElement('li');
+        let img=document.createElement('img');
+         img.src=ele.urls.small;
+         console.log(img.src);
+         li.append(img);
+         imgBox.append(li);
+      })
+
+      
 
     }
     xhr.onerror=function (){
